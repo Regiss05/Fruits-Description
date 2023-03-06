b109d99aea07d1171509541da1e2c548b6272016
regiss05 <regiss.steven@gmail.com>
2023-03-06 13:05:38 +0200

    add phone

---
+++ Components/phone.tsx
@@ -0,0 +1,29 @@
+export const phonestore = {
+    phones: [
+        {
+            title: 'iphone 4',
+            // image:
+            description: 'good phone' 
+        },
+        {
+            title: 'iphone 5',
+            // image:
+            description: 'good phone' 
+        },
+        {
+            title: 'iphone 6',
+            // image:
+            description: 'good phone' 
+        },
+        {
+            title: 'iphone 7',
+            // image:
+            description: 'good phone' 
+        },
+        {
+            title: 'iphone 8',
+            // image:
+            description: 'good phone' 
+        }
+    ]
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import { phonestore } from '@/Components/phone'
+import { useState } from 'react'
+import Modal from 'react-modal'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [data, setData] = useState()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const addingData = () => {
+    setData(phonestore);
+    console.log(phonestore);
+  }
+
+  const customStyles = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.6)'
+    },
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  }
+
   return (
     <>
       <Head>
@@ -13,7 +39,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-I'm regiss mukubiza
+        <button onClick={() => setIsOpen(true)}>Add User</button>
+        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+          {/* <input>Add Phone Name</input>
+        <input>Description of the phone</input> */}
+          <button onClick={() => setIsOpen(false)}>Close Modal</button>
+          <form>
+            <label for="phone_name">Phone name</label>
+            <input type="text" />
+            <label for="description">Description</label>
+            <input type="text" />
+          </form>
+
+        </Modal>
+
+        <button onClick={() => addingData()}>Click Me</button>
+
+        {
+          data && data.phones.map((b, index) => {
+            // if(b.category.toLowerCase() == "java") {
+            return (
+              <div key={index}>
+                <h1>{b.title}</h1>
+                <p>{b.description}</p>
+
+
+                <button>Delete</button>
+              </div>
+            )
+            // }
+          })
+        }
+
       </main>
     </>
   )
