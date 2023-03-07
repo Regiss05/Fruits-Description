fdea1affc37e752e9d214d2d2839e1aba3e45abe
regiss05 <regiss.steven@gmail.com>
2023-03-07 10:10:56 +0200

    add search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import Link from 'next/link';
+import SearchBar from './SearchBar';
 
 export default function Home() {
   const [userInput, setUserInput] = useState('');
@@ -14,15 +15,15 @@ export default function Home() {
   }
 
   const addName = () => {
-    if (setNameList){
+    if (setNameList) {
       setNameList([
         userInput,
         ...nameList
       ])
-  
+
       setUserInput('')
     }
-   
+
   }
 
   const handleDelete = (name) => {
@@ -40,49 +41,64 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <div class="flex flex-col justify-center items-center">
-        <div className='w-2/4 text-center bg-primary'>
-          <h1 className='text-2xl my-5'>ADD STOCK</h1>
-          <div>
-            <input type="text"
-              value={userInput}
-              onChange={handleChange}
-              placeholder='Add new name'
-              className=' block w-full p-2.5'
-            />
-            <button onClick={(e) => {
-              e.preventDefault
-              addName()
-            }}
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            >
-              Add Name
-            </button>
-          </div>
-          {
-            nameList.length >= 1 ? nameList.map((name, idx) => {
-              return (
-                // eslint-disable-next-line react/jsx-key
-                <div className='border'>
-                  <div key={idx}>
-                    <h1 className=''>{name}</h1>
-                    {/* <p>je sui vraiment ct</p> */}
+        <div class="flex flex-col justify-center items-center">
+
+          {/* <form>
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              </div>
+              <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+                <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+          </form> */}
+
+          <div className='w-2/4 text-center bg-primary'>
+            <SearchBar />
+            <h1 className='text-2xl my-5'>ADD STOCK</h1>
+            <div>
+              <input type="text"
+                value={userInput}
+                onChange={handleChange}
+                placeholder='Add new name'
+                className=' block w-full p-2.5'
+              />
+              <button onClick={(e) => {
+                e.preventDefault
+                addName()
+              }}
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              >
+                Add Name
+              </button>
+            </div>
+            <div className=''>
+            {
+              nameList.length >= 1 ? nameList.map((name, idx) => {
+                return (
+                  // eslint-disable-next-line react/jsx-key
+                  <div className='border'>
+                    <div key={idx}>
+                      <h1 className=''>{name}</h1>
+                      {/* <p>je sui vraiment ct</p> */}
+                    </div>
+                    <Link href="/items"
+                      className='class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"'
+                    >View</Link>
+                    <button onClick={(e) => {
+                      e.preventDefault
+                      handleDelete(name)
+                    }}
+                      className='type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
+                    >Delete</button>
                   </div>
-                  <Link href="/items"
-                  className='class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"'
-                  >View</Link>
-                  <button onClick={(e) => {
-                    e.preventDefault
-                    handleDelete(name)
-                  }}
-                  className='type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
-                  >Delete</button>
-                </div>
-              )
-            })
-              : ''
-          }
-        </div>
+                )
+              })
+                : ''
+            }
+            </div>
+          </div>
         </div>
       </main>
     </>
